771c444c12dd26ac26c19e1ba0a01d7cc3ad9d7d
shafarizkyf <shafarizkyf@gmail.com>
2023-06-04 17:45:18 +0700

    update map

--- src/map.js
+++ src/map.js
@@ -1,18 +1,163 @@
 export const MAP = [
   {
     "name": "go",
-    "amount": 200,
+    "amount": 4440,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "community_chest",
+    "amount": 0,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "tax_income",
+    "amount": -8600,
+  },
+  {
+    "name": "reading_railroad",
+    "amount": 1000,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "chance",
+    "amount": 0,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
   },
   {
     "name": "visit_jail",
     "amount": 0,
   },
   {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "electric_company",
+    "amount": 512,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "reading_railroad",
+    "amount": 1000,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "community_chest",
+    "amount": 0,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
     "name": "free_parking",
     "amount": 0,
   },
   {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "chance",
+    "amount": 0,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "reading_railroad",
+    "amount": 1000,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "water_works",
+    "amount": 516,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
     "name": "jail",
     "amount": -50
-  }
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "community_chest",
+    "amount": 512,
+  },
+
+  {
+    "name": "place",
+    "amount": 1600,
+  },
+  {
+    "name": "reading_railroad",
+    "amount": 1000,
+  },
+  {
+    "name": "chance",
+    "amount": 1600,
+  },
+  {
+    "name": "place",
+    "amount": 0,
+  },
+  {
+    "name": "luxury_tax",
+    "amount": 8600,
+  },
+  {
+    "name": "place",
+    "amount": 1600,
+  },
 ];
